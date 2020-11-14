775d29673e54c6757607019b2b9a93397822e880
Larry X <papadocarock@gmail.com>
2020-11-14 11:42:54 -0500

    build

---
+++ client/build/precache-manifest.3880b5a579bb5d70f5cf8ec6094ff35d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61a9b60db927941abd3b05f661af9aff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3868f71d0dde12bbee70",
+    "url": "/static/css/main.96b94756.chunk.css"
+  },
+  {
+    "revision": "39288cb8ba2c6b5919e3",
+    "url": "/static/js/2.2081cfa9.chunk.js"
+  },
+  {
+    "revision": "9be8f09f0bfe1d20164d4c42e389b466",
+    "url": "/static/js/2.2081cfa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3868f71d0dde12bbee70",
+    "url": "/static/js/main.b07ee172.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
